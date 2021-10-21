417df88cb300edbb3e42cebd333c38ff1a786504
Jesus Carpintero <jesus.carpintero@adevinta.com>
2021-10-21 12:51:45 +0200

    wip

--- db/postgres-start.sh
+++ db/postgres-start.sh
@@ -4,4 +4,4 @@
 
 docker run --name vulcan-scan-enginedb -p 5434:5434 -e POSTGRES_PASSWORD=vulcan -e POSTGRES_USER=vulcan -e POSTGRES_DB=scan-enginedb -e PGPORT=5434 -d --rm postgres:13.3-alpine
 
-while ! nc -z localhost 5432; do echo "Waiting for postgres" && sleep 2; done;
+while ! nc -z localhost 5434; do echo "Waiting for postgres" && sleep 2; done;
