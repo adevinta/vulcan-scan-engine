8ba831fa6ff9652c377ee8a4aa60c299d53323ac
Jesus Carpintero <jesus.carpintero@adevinta.com>
2021-10-21 12:47:53 +0200

    more stuff

--- db/clean.sh
+++ db/clean.sh
@@ -2,4 +2,5 @@
 
 # Copyright 2021 Adevinta
 
-docker run --net=host --rm -v $(pwd):/flyway/sql flyway/flyway:${FLYWAY_VERSION:-7} -user=vulcan -password=vulcan -url=jdbc:postgresql://localhost:5434/scan-enginedb -baselineOnMigrate=true clean
+docker run --net=host --rm -v "$PWD":/flyway/sql flyway/flyway:"${FLYWAY_VERSION:-8}" \
+    -user=vulcan -password=vulcan -url=jdbc:postgresql://localhost:5434/scan-enginedb -baselineOnMigrate=true clean
--- db/postgres-start.sh
+++ db/postgres-start.sh
@@ -2,4 +2,6 @@
 
 # Copyright 2021 Adevinta
 
-docker run --name vulcan-scan-enginedb -p 5434:5434 -e POSTGRES_PASSWORD=vulcan -e POSTGRES_USER=vulcan -e POSTGRES_DB=scan-enginedb -e PGPORT=5434 -d --rm postgres:9.6-alpine
+docker run --name vulcan-scan-enginedb -p 5434:5434 -e POSTGRES_PASSWORD=vulcan -e POSTGRES_USER=vulcan -e POSTGRES_DB=scan-enginedb -e PGPORT=5434 -d --rm postgres:13.3-alpine
+
+while ! nc -z localhost 5432; do echo "Waiting for postgres" && sleep 2; done;
