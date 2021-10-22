fea578c0fe2580dd261cc2692b0dc1416b3067b4
Jesus Carpintero <jesus.carpintero@adevinta.com>
2021-10-22 09:40:03 +0200

    Use alpine flyway

--- db/clean.sh
+++ db/clean.sh
@@ -2,5 +2,5 @@
 
 # Copyright 2021 Adevinta
 
-docker run --net=host --rm -v "$PWD":/flyway/sql flyway/flyway:"${FLYWAY_VERSION:-8}" \
+docker run --net=host --rm -v "$PWD":/flyway/sql flyway/flyway:"${FLYWAY_VERSION:-8}-alpine" \
     -user=vulcan -password=vulcan -url=jdbc:postgresql://localhost:5434/scan-enginedb -baselineOnMigrate=true clean
--- run.sh
+++ run.sh
@@ -22,6 +22,6 @@ fi
 
 flyway -user="$PG_USER" -password="$PG_PASSWORD" \
   -url="jdbc:postgresql://$PG_HOST:$PG_PORT/$PG_NAME?sslmode=$PG_SSLMODE" \
-  -baselineOnMigrate=true -locations=filesystem:/app/sql migrate
+  -community -baselineOnMigrate=true -locations=filesystem:/app/sql migrate
 
 ./vulcan-scan-engine -c run.toml
