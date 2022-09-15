b5361ecb190001c51ca6b2e52b1e6092cbf1961e
Gerard Guillemas Martos <gerard@guillemas.com>
2022-09-15 16:25:48 +0200

    Update Flyway

--- db/repair_migrations.sh
+++ db/repair_migrations.sh
@@ -2,5 +2,5 @@
 
 # Copyright 2021 Adevinta
 
-docker run --net=host --rm -v "$PWD":/flyway/sql flyway/flyway:"${FLYWAY_VERSION:-8}-alpine" \
+docker run --net=host --rm -v "$PWD":/flyway/sql flyway/flyway:"${FLYWAY_VERSION:-9}-alpine" \
     -user=vulcan -password=vulcan -url=jdbc:postgresql://localhost:5434/scan-enginedb -baselineOnMigrate=true repair
