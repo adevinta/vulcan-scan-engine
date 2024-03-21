d7b716d6f9cd7ab75bc4a84d541982271f9e66c9
Jesús Carpintero <jesus.carpintero@adevinta.com>
2024-03-21 10:15:10 +0100

    Bump flyway (#172)

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 # Copyright 2021 Adevinta
 
+set -e
+
 # export default values for required vars if not set
 export PORT=${PORT:-8080}
 export DOGSTATSD_ENABLED=${DOGSTATSD_ENABLED:-false}
@@ -12,7 +14,7 @@ export CHECKS_SQS_TIMEOUT=${CHECKS_SQS_TIMEOUT:-30}
 export CHECKS_CREATOR_CHECKPOINT=${CHECKS_CREATOR_CHECKPOINT:-100}
 export PERSISTENCE_CACHE=${PERSISTENCE_CACHE:-120}
 
-# Nessus section  will be deprecated, 
+# Nessus section  will be deprecated,
 # We add this for compatibility using the new dynamic method.
 export QUEUES_NESSUS_CHECKTYPES=${QUEUES_NESSUS_CHECKTYPES:-'[]'}
 
@@ -41,6 +43,6 @@ fi
 
 flyway -user="$PG_USER" -password="$PG_PASSWORD" \
   -url="jdbc:postgresql://$PG_HOST:$PG_PORT/$PG_NAME?sslmode=$PG_SSLMODE" \
-  -community -baselineOnMigrate=true -locations=filesystem:/app/sql migrate
+  -baselineOnMigrate=true -locations=filesystem:/app/sql migrate
 
 exec ./vulcan-scan-engine -c run.toml
