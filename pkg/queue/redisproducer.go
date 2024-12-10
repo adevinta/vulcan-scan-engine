d93e108464af603f72f929cc2e79126ba65ba9b4
Jesús Carpintero <jesus.carpintero@gmail.com>
2024-12-10 18:30:25 +0100

    Set timeout and retries

--- pkg/queue/redisproducer.go
+++ pkg/queue/redisproducer.go
@@ -6,6 +6,7 @@ package queue
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
@@ -44,7 +45,7 @@ func NewRedisProducer(config RedisConfig, log log.Logger) *RedisProducer {
 // SendMessage sends a message to the producer defined queue.
 func (s *RedisProducer) SendMessage(queueName string, body []byte) error {
 	task := asynq.NewTask("checks", body)
-	m, err := s.client.Enqueue(task, asynq.Queue(queueName))
+	m, err := s.client.Enqueue(task, asynq.Queue(queueName), asynq.Timeout(10*time.Hour), asynq.MaxRetry(3))
 	level.Debug(s.log).Log("asynqId", m.ID, "queue", m.Queue)
 	return err
 }
